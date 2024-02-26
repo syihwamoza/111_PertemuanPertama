49b7c93a86b9c9a1fc8ab1d65528076a993d6925
syihwamoza <mozakastella26@gmail.com>
2024-02-26 11:53:58 +0700

    masukkan dan menampilkan luas

--- PraktikumPertamaPemogramanDasar/PraktikumPertamaPemogramanDasar.cpp
+++ PraktikumPertamaPemogramanDasar/PraktikumPertamaPemogramanDasar.cpp
@@ -15,7 +15,11 @@ int main()
     //end
 
     double nJejari, nLuas; 
-    cout << "mMasukan Jejari = "; 
+    cout << "Masukan Jejari = "; 
+    cin >> nJejari; 
+    nLuas = 3, 14 * nJejari * nJejari;
+    cout << "Luasnya = " << nLuas << endl;
+
     system("pause"); 
 }
 
